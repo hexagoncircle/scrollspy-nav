e9dba9e7d832e9df522fbbad930bc64518041207
Ryan Mulligan <hexagoncircle@gmail.com>
2023-09-16 14:00:21 -0700

    Update directory

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "_site",
+      output: "dist",
       includes: "_includes",
       layouts: "_layouts",
       data: "_data",
