1933dac4fbd4cdb68c908bfb07af07d99898d2da
Ryan Mulligan <hexagoncircle@gmail.com>
2023-09-16 13:45:05 -0700

    Add scrollspy-nav component and examples

---
+++ eleventy.config.js
@@ -0,0 +1,19 @@
+const pluginWebc = require("@11ty/eleventy-plugin-webc");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginWebc, {
+    components: "src/_includes/**/*.webc",
+  });
+
+  eleventyConfig.addLayoutAlias("base", "base.webc");
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+    },
+  };
+};
