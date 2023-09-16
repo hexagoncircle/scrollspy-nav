0e9c1ae3e65d2f814329a6e172a6b3e8cd167870
Ryan Mulligan <hexagoncircle@gmail.com>
2023-09-16 14:04:15 -0700

    Update ouput directory name

--- eleventy.config.js
+++ eleventy.config.js
@@ -10,7 +10,7 @@ module.exports = function (eleventyConfig) {
   return {
     dir: {
       input: "src",
-      output: "dist",
+      output: "docs",
       includes: "_includes",
       layouts: "_layouts",
       data: "_data",
